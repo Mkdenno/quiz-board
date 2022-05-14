f8a4043e49451b1ed94e48e6fc2d0c4fa5c095fd
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-14 20:28:25 +0300

    It will get elementById in Js file

---
+++ assets/JS/script.js
@@ -0,0 +1,13 @@
+
+var userAnswers=[];
+
+document.getElementById("question").addEventListener('submit', (e )=>{
+
+
+const question1=document.getElementById("q_one");
+const question2=document.getElementById("q_two");
+const question3=document.getElementById("q_three");
+const question4=document.getElementById("q_four");
+const question5=document.getElementById("q_five");
+
+//pushing users answer to empty array
