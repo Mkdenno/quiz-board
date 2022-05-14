e02c731209d25ec45fc53063bfd009290f14e480
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-05-14 20:30:33 +0300

    it will push the users checked answers to an empty array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,3 +11,44 @@ const question4=document.getElementById("q_four");
 const question5=document.getElementById("q_five");
 
 //pushing users answer to empty array
+for(let i=0; i<userAnswers.length; i++)
+{
+    if(question1[i].checked)
+    {
+        userAnswers.push(question1[i].value);
+    }
+}
+
+for(let i=0; i<userAnswers.length; i++)
+{
+    if(question2[i].checked)
+    {
+        userAnswers.push(question2[i].value);
+    }
+}
+
+for(let i=0; i<userAnswers.length; i++)
+{
+    if(question3[i].checked)
+    {
+        userAnswers.push(question3[i].value);
+    }
+}
+
+for(let i=0; i<userAnswers.length; i++)
+{
+    if(question4[i].checked)
+    {
+        userAnswers.push(question4[i].value);
+    }
+}
+
+for(let i=0; i<userAnswers.length; i++)
+{
+    if(question5[i].checked)
+    {
+        userAnswers.push(question5[i].value);
+    }
+}
+
+})
